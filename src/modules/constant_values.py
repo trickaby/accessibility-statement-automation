5876340434b4445081487052c8df24dd1018b919
Timin Rickaby <timin.rickaby@homeoffice.gov.uk>
2024-12-20 13:30:46 +0000

    update functionality, add new features for WCAG, non-accessible content

--- src/modules/constant_values.py
+++ src/modules/constant_values.py
@@ -5,15 +5,8 @@ root_dir = Path(__file__).resolve().parent.parent.parent
 input_path = "tests/data/input.csv"
 output_path = "src/data/output.csv"
 
-output_columns = [
-    "URL", "Date Prepared By", "Date Last Reviewed", "Date Last Tested",
-    "Days Since Last Tested", "Who Tested By", "Feedback Header Present",
-    "Reporting Problems Header Present", "Enforcement Procedure Header Present",
-    "Compliance Status"
-]
-
 output_date_format = "%d/%m/%Y" #DD/MM/YYYY
 
-partially_compliant_format = "Partial"
-fully_compliant_format = "Full"
-non_compliance_format = "None"
+partially_compliant_format = "Partially compliant"
+fully_compliant_format = "Fully compliant"
+non_compliance_format = "Non-compliant"
