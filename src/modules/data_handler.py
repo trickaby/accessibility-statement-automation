3dfeb8199f03d5a311cb30a953c5acc3f66038a7
Timin Rickaby <timin.rickaby@homeoffice.gov.uk>
2024-12-20 13:59:15 +0000

    utf-8 encoding

--- src/modules/data_handler.py
+++ src/modules/data_handler.py
@@ -9,7 +9,7 @@ def read_input_csv(input_path):
 
 def write_output_csv(output_path, data, output_columns):
     path = Path(root_dir, output_path)
-    with open(path, 'w', newline='') as csv_file:
+    with open(path, 'w', newline='', encoding="utf-8") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=output_columns)
         writer.writeheader()
         writer.writerows(data)
